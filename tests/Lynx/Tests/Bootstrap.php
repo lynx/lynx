d3b0ae47f67026b033f378564bba98b1f17393f4
Dmitry Patsura <talk@dmtry.me>
2015-11-23 22:51:25 +0300

    [Tests] Tmp fix for Doctrine ORM mapping annotations loading

--- tests/Lynx/Tests/Bootstrap.php
+++ tests/Lynx/Tests/Bootstrap.php
@@ -10,3 +10,8 @@ include_once __DIR__ . '/Utils.php';
 include_once __DIR__ . '/Models/User.php';
 include_once __DIR__ . '/Models/Product.php';
 include_once __DIR__ . '/Models/Group.php';
+
+class_exists('Doctrine\ORM\Mapping\Entity', true);
+class_exists('Doctrine\ORM\Mapping\Column', true);
+class_exists('Doctrine\ORM\Mapping\Id', true);
+class_exists('Doctrine\ORM\Mapping\Table', true);
