82c0f64c3a490af7535d71224e69a3e3625d8914
Dmitry Patsura <talk@dmtry.me>
2015-11-23 23:33:51 +0300

    [Tests] Test possible bug with clone on hydration

--- tests/Lynx/Tests/RepositoryTest.php
+++ tests/Lynx/Tests/RepositoryTest.php
@@ -16,13 +16,14 @@ class RepositoryTest extends TestCase
         $repository = $this->em->getRepository(User::class);
 
         /** @var User $result */
-        $result = $repository->getOne(1);
+        $userOne = $repository->getOne(1);
+        static::assertSuccessUser($userOne);
 
-        static::assertInstanceOf(User::class, $result);
-        static::assertInternalType('integer', $result->id);
-        static::assertInternalType('string', $result->name);
-        static::assertInstanceOf(DateTime::class, $result->dateCreated);
-        static::assertInternalType('integer', $result->groupId);
+        /** @var User $result */
+        $userTwo = $repository->getOne(2);
+        static::assertSuccessUser($userTwo);
+
+        static::assertNotEquals(spl_object_hash($userOne), spl_object_hash($userTwo));
     }
 
     public function testGetOneMethodNotFoundForUserEntity()
@@ -30,5 +31,14 @@ class RepositoryTest extends TestCase
         /** @var \Lynx\Repository $repository */
         $repository = $this->em->getRepository(User::class);
         static::assertNull($repository->getOne(100000000));
+    }
+
+    protected static function assertSuccessUser($result)
+    {
+        static::assertInstanceOf(User::class, $result);
+        static::assertInternalType('integer', $result->id);
+        static::assertInternalType('string', $result->name);
+        static::assertInstanceOf(DateTime::class, $result->dateCreated);
+        static::assertInternalType('integer', $result->groupId);
     }
 }
