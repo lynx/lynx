d27ec1aab60231b30688607fbc36ec91430ed07e
Dmitry Patsura <talk@dmtry.me>
2015-11-23 22:25:41 +0300

    Oops! Use static

--- tests/Lynx/Tests/EntityManagerTest.php
+++ tests/Lynx/Tests/EntityManagerTest.php
@@ -10,6 +10,6 @@ class EntityManagerTest extends TestCase
     public function testGetRepositorySuccess()
     {
         $result = $this->em->getRepository('Model\User');
-        $this->assertInstanceOf(\Lynx\Repository::class, $result);
+        static::assertInstanceOf(\Lynx\Repository::class, $result);
     }
 }
