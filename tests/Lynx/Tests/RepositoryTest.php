d812bb79200d3b2cced85c2a3d7e89cf68a4a0f1
Dmitry Patsura <talk@dmtry.me>
2015-11-23 22:59:48 +0300

    [Tests] Implement full success test for Repository - getOne method

--- tests/Lynx/Tests/RepositoryTest.php
+++ tests/Lynx/Tests/RepositoryTest.php
@@ -5,15 +5,22 @@
 
 namespace Lynx\Tests;
 
+use Model\User;
+
 class RepositoryTest extends TestCase
 {
     public function testGetOneMethodForUser()
     {
         /** @var \Lynx\Repository $repository */
         $repository = $this->em->getRepository('Model\User');
+
+        /** @var User $result */
         $result = $repository->getOne(1);
 
         static::assertInstanceOf(\Model\User::class, $result);
-        static::assertInternalType('string', $result->firstname);
+        static::assertInternalType('integer', $result->id);
+        static::assertInternalType('string', $result->name);
+        static::assertInstanceOf(\DateTime::class, $result->dateCreated);
+        static::assertInternalType('integer', $result->group_id);
     }
 }
