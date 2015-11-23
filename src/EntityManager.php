113f4987800105ce17d3e5a24c3977d4d0844411
Dmitry Patsura <talk@dmtry.me>
2015-11-23 23:34:59 +0300

    Up code quality :smiley_cat:

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -38,7 +38,7 @@ class EntityManager
 
     /**
      * @param Connection $connection
-     * @param Cache $cache
+     * @param Configuration $configuration
      */
     public function __construct(Connection $connection, Configuration $configuration)
     {
@@ -93,7 +93,7 @@ class EntityManager
     }
 
     /**
-     * @param $className
+     * @param string $className
      * @return object
      */
     public function getObject($className)
