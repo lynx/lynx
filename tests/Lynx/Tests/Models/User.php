08253edd909c14426436d4c85a75b3e798e74d66
Dmitry Patsura <talk@dmtry.me>
2015-11-23 22:51:05 +0300

    [Tests] Fix mapping for Model\User

--- tests/Lynx/Tests/Models/User.php
+++ tests/Lynx/Tests/Models/User.php
@@ -6,40 +6,34 @@
 
 namespace Model;
 
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\JoinColumn;
-use Doctrine\ORM\Mapping\OneToOne;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @entity
- * @table(name="users")
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="users"
+ * );
  */
 class User
 {
     /**
-     * @Id
-     * @column(type="integer", length=11, name="id")
+     * @ORM\Id
+     * @ORM\Column(type="integer", length=11, name="id")
      */
     public $id;
 
     /**
-     * @column(name="date_created", type="date")
+     * @ORM\Column(name="date_created", type="date")
      */
     public $dateCreated;
 
     /**
-     * @column(length=50, , name="name")
+     * @ORM\Column(length=50, name="name")
      */
     public $name;
 
     /**
-     * @column(type="integer", length=11, name="group_id")
+     * @ORM\Column(type="integer", length=11, name="group_id")
      */
     public $group_id;
-
-    /**
-     * @OneToOne(targetEntity="Model\Group")
-     * @JoinColumn(name="group_id", referencedColumnName="id")
-     */
-    public $Group;
 }
