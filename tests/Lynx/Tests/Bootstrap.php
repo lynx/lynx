f65d12e2ff8dbff912d8d279baa1c103b06f8e82
Dmitry Patsura <talk@dmtry.me>
2015-11-24 23:36:30 +0300

    [Tests] Remove unneeded class

--- tests/Lynx/Tests/Bootstrap.php
+++ tests/Lynx/Tests/Bootstrap.php
@@ -5,7 +5,6 @@ namespace Lynx\Tests;
 include_once __DIR__ . '/../../../vendor/autoload.php';
 
 include_once __DIR__ . '/TestCase.php';
-include_once __DIR__ . '/Utils.php';
 
 include_once __DIR__ . '/Models/User.php';
 include_once __DIR__ . '/Models/Product.php';
